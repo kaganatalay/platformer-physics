d1dc266a7cec675846efba449762919fa23bf9d2
kaganatalay <48818679+kaganatalay@users.noreply.github.com>
2020-12-22 20:20:18 +0300

    camera follow

--- sketch.js
+++ sketch.js
@@ -8,11 +8,11 @@ function setup() {
   TilemapParser({
     src: 
     [
-      [1, 1, 1, 1, 1, 1],
-      [0, 0, 0, 0, 0, 1],
-      [0, 0, 0, 0, 1, 1],
-      [0, 0, 0, 1, 1, 1],
-      [1, 1, 1, 1, 1, 1]
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+      [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+      [0, 0, 0, 1, 1, 1, 0, 0, 0, 1],
+      [1, 1, 1, 1, 1, 1, 0, 0, 0, 0]
     ],
     
     size: createVector(160, 160)
@@ -22,7 +22,9 @@ function setup() {
 function draw() {
   background(51);
   
-
+  push();
+  translate(width/2 - player.position.x, height/2 - player.position.y);
+  
   for(let platform of platforms) {
     platform.render();
     for(let body of bodies) {
@@ -32,7 +34,7 @@ function draw() {
 
   player.start();
 
-  
+  pop();
   
 }
 
